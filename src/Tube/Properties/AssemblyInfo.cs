1c92732ee208e0ec7d91b186127d2634c88e85aa
Charles Ridley <charlieridley@gmail.com>
2013-05-09 13:40:03 -0400

    update version

--- src/Tube/Properties/AssemblyInfo.cs
+++ src/Tube/Properties/AssemblyInfo.cs
@@ -7,5 +7,5 @@ using System.Runtime.InteropServices;
 // associated with an assembly.
 [assembly: AssemblyTitle("Tube")]
 [assembly: Guid("9b727c0a-285b-46ad-9dad-9a1f1eec12bd")]
-[assembly: AssemblyVersion("0.1.9")]
-[assembly: AssemblyFileVersion("0.1.9")]
+[assembly: AssemblyVersion("0.1.10")]
+[assembly: AssemblyFileVersion("0.1.10")]
